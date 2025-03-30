6647265dfa912e081e99c2871d1d4ea5b3150a08
andrew <andrew.octavious@gmail.com>
2025-03-30 18:59:01 +0700

    streamlit fix

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -8,7 +8,7 @@ import os
 st.title("Analisis jumlah User Rental Bike")
 st.subheader("Analisis jumlah user reantal bike berdasarkan keadaan lingkungan")
 
-main_df = pd.read_csv(os.path.join(os.path.dirname(__file__), "main-data.csv"), parse_dates=['dteday'])
+main_df = pd.read_csv(os.path.join(os.path.dirname(__file__), "main_data.csv"), parse_dates=['dteday'])
 main_df.rename(columns={
     "cnt": "Total_Customer",
     "atemp": "Temperature",
